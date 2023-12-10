9e257ec3aefc711ad30a4619088f26f28d54fb9c
Larissa Iovanovich <117244238+LarissaIovanovich@users.noreply.github.com>
2023-12-10 01:45:05 -0300

    Add files via upload

--- src/app.js
+++ src/app.js
@@ -1,18 +1,23 @@
-import express from "express";                                     //o mongo é o nosso banco e o mongoose é a biblioteca que está fazendo essa interface entre o mongo e a API.
-import conectaNaDataBase from "./config/dbConnect.js";
-import routes from "./routes/index.js";
+import express from "express";
+import conectaNaDataBase from "./src/config/dbConnect.js";
+import routes from "./src/routes/index.js";
+import swaggerUi from 'swagger-ui-express';
+import swaggerFile from './swagger_doc.json' assert { type: 'json' };
 
-const conexao = await conectaNaDataBase(); //precisa do await pq é assincrono
+const app = express();
+
+const conexao = await conectaNaDataBase();
 
 conexao.on("error", (erro) => {
     console.error("Erro de conexão", erro);
-})
+});
 
 conexao.once("open", () => {
     console.log("Conexão feita com sucesso!");
-})
+});
 
-const app = express();
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerFile));
+
 routes(app);
 
 export default app;
